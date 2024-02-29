3ae9b675554171b724619323b7b94a60f1226fdd
emrea <emreakburak94@gmail.com>
2024-02-29 17:29:22 +0300

    Test2

--- src/main/java/com/example/application/test/SmokeTest.java
+++ src/main/java/com/example/application/test/SmokeTest.java
@@ -6,11 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SmokeTest {
 
-
+    //test
     @Test
     void contextLoads() throws Exception {
 
         System.out.println("Test Runned");
-        
+
     }
 }
